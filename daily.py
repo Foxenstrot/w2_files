eec28affeb9f6f544506038e6ee55459c16c9b53
Nils <nilekl0117@skola.goteborg.se>
2024-09-04 16:38:03 +0200

    Made the loop much cooler

--- daily.py
+++ daily.py
@@ -8,7 +8,8 @@ all_lines: list[str] = []
 for filename in files:
     with open(filename, "r") as file:
         for line in file:
-            all_lines.append(line)
+            while (line := file.readline()):
+                all_lines.append(line.strip())
 
 selected: int = randrange(0, len(all_lines))
 print(all_lines[selected])
